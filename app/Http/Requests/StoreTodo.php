80527ae2b39179266ab8dc43f1f4878fc5f2bae2
vicente <vicente19981@live.com>
2024-11-15 18:10:23 -0300

    feat: criando Request para criar todo

---
+++ app/Http/Requests/StoreTodo.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\Status;
+use App\Services\DTO\Todo\Create\TodoCreateInputDto;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTodo extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'min:3',
+                'max:25',
+            ],
+            'description' => [
+                'required',
+                'string',
+                'min:3',
+                'max:255',
+            ],
+            'status' => [
+                'string',
+                'in:pending,completed'
+            ],
+            'category_id' => [
+                'required',
+                'string',
+                'exists:categories,id'
+            ]
+        ];
+    }
+
+    public function toDTO(): TodoCreateInputDto
+    {
+        return new TodoCreateInputDto(
+            name: $this->input('name'),
+            description: $this->input('description'),
+            status: $this->input('status'),
+            category_id: $this->input('category_id'),
+        );
+    }
+}
