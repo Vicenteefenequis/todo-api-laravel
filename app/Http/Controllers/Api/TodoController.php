e3e3e016005bbefe92f0b1cbc0ef7cf4217c106d
vicente <vicente19981@live.com>
2024-11-15 18:37:20 -0300

    feat: adicionando controller index para listagem de todos

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -6,15 +6,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTodo;
 use App\Services\Todo\CreateTodoService;
-use Illuminate\Http\Request;
+use App\Services\Todo\ListTodosServices;
+use Illuminate\Http\JsonResponse;
 
 class TodoController extends Controller
 {
-    public function index(Request $request) {
-
+    public function index(ListTodosServices $listTodosServices): JsonResponse
+    {
+        $output = $listTodosServices->execute();
+        return response()->json($output);
     }
 
-    public function store(StoreTodo $request, CreateTodoService $createTodoService) {
+    public function store(StoreTodo $request, CreateTodoService $createTodoService): JsonResponse
+    {
         $output = $createTodoService->execute($request->toDTO());
         return response()->json($output, 201);
     }
