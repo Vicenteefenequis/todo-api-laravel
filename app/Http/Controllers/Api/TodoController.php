a60062af1e7fd40fba6eb3e867ee4c3bdf833558
vicente <vicente19981@live.com>
2024-11-16 10:33:41 -0300

    feat: adicionando controller para atualizar categoria

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTodo;
+use App\Http\Requests\UpdateTodo;
 use App\Services\DTO\Todo\TodoInputDto;
 use App\Services\Todo\CreateTodoService;
 use App\Services\Todo\DeleteTodoService;
 use App\Services\Todo\ListTodoService;
 use App\Services\Todo\ListTodosService;
+use App\Services\Todo\UpdateTodoService;
 use Illuminate\Http\JsonResponse;
 
 class TodoController extends Controller
@@ -32,9 +34,16 @@ class TodoController extends Controller
         return response()->json($output, 201);
     }
 
-    public function destroy(string $id, DeleteTodoService $deleteTodoService)
+    public function destroy(string $id, DeleteTodoService $deleteTodoService): JsonResponse
     {
         $output = $deleteTodoService->execute(new TodoInputDto(id: $id));
+        return response()->json($output);
+    }
+
+    public function update(string $id, UpdateTodo $request, UpdateTodoService $updateTodoService): JsonResponse
+    {
+        $output = $updateTodoService->execute($request->toDTO());
+
         return response()->json($output);
     }
 }
